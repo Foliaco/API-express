eb2730c1a8d0755668489f6c63800fde4c913a97
miguel_foliaco <foliaco18@gmail.com>
2021-08-26 21:55:52 -0500

    añadi el cors

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express =require('express');
 const app=express();
 const router=require('./router')
 const morgan=require('morgan')
+const cors=require('cors')
 //middleware
 app.use(express.urlencoded({extended:false}))
 app.use(express.json());
 app.use(morgan('dev'))
+app.use(cors())
 //router
 app.use('/',router)
 app.set('port',process.env.PORT||4000);
